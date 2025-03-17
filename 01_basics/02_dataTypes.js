0232c2fe5025e56d603b05dfe566adad6c2c2589
Farhan Akhtar <123282333+faroutnerd@users.noreply.github.com>
2025-03-17 10:20:57 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";   // treat all JS code as newer version
+
+// alert(3+3);     // We are using NodeJS not browser
+
+// console.log(3+3)    console.log(3+4)     // Will show an error
+
+console.log(3+3);   console.log(3+3)
+
+let name = "Farhan"
+let age = 23
+let isLoggedIn = false;
+
+// number => 2 to the power 53\
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (suppose you want to state the temperature and you cannot use 0 instead you got a value null when there is no data is available to show)
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof null)        // object
+console.log(typeof undefined)        // undefined
