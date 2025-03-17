f328631fc5a12a1544976f4ec9a778a95cac070a
Farhan Akhtar <123282333+faroutnerd@users.noreply.github.com>
2025-03-17 10:03:27 +0000

    variable questions

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -7,7 +7,11 @@ const accountId = 144553
 let accountEmail = "farhan@gmail.com"   // scope variable don't have any effect outside the scope.
 var accountPassword = "12345"       // scope variable ouside gets changed
 accountCity = "Kolkata"     // Avoid using such kind of variable declaration
-let accountState;
+/*
+if you do like this then it will create a global variable and won't show any error
+but if you use the same in strict mode it will show an error accountCity is not defined
+*/
+let accountState;   // undefined
 
 // accountId = 14   NOT ALLOWED as it is a constant
 accountEmail = "gitfarhan@gmail.com"   // Allowed
@@ -20,3 +24,31 @@ console.log(accountPassword);
 console.log(accountCity);
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Basic Questions
+1. What are the different ways to declare variables in JavaScript?
+2. What is the difference between var, let, and const?
+3. What happens if you declare a variable without var, let, or const?
+4. Can you redeclare a variable using var, let, and const?
+5. What is hoisting, and how does it affect variables?
+6. What is the scope of var, let, and const in JavaScript?
+
+Intermediate Questions;
+7. What is a temporal dead zone (TDZ) in JavaScript?
+8. What happens when you try to modify a const variable?
+9. Is JavaScript statically or dynamically typed? How does it relate to variables?
+10. What are global and local variables in JavaScript?
+11. How does var behave inside a function?
+12. What is the difference between function scope and block scope?
+13. How do JavaScript closures affect variable accessibility?
+14. What happens when you try to access a variable before it is declared?
+
+Advanced Questions:
+15. Can you declare a variable without assigning a value? What happens?
+16. What are the differences between undefined, null, and not defined?
+17. What are shadowing and variable masking in JavaScript?
+18. How does JavaScript handle variable memory allocation?
+19. What is destructuring, and how does it work with variables?
+20. How do let and const help in writing better asynchronous code?
+*/
