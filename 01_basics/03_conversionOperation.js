c40ed8d3f1b2a7c2547939c0cdddfbce2a341762
Farhan Akhtar <123282333+faroutnerd@users.noreply.github.com>
2025-03-19 08:43:42 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -165,3 +165,53 @@ const typeConversionTable = [
   
   console.table(typeConversionTable);
   
+
+  // *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);    // 4
+// console.log(2-2);    // 0
+// console.log(2*2);    // 4
+// console.log(2**3);   // 8
+// console.log(2/3);    // 0.66666666..
+// console.log(2%3);    // 2
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3); "hello hitesh"
+
+// console.log("1" + 2);   // 12
+// console.log(1 + "2");   // 12
+// console.log("1" + 2 + 2);   // 122
+
+// console.log(1 + 2 + "2");   // 32     -> IMPORTANT
+
+// console.log( (3 + 4) * 5 % 3);   // 2
+
+console.log(true);  // true
+console.log(+true); // 1    -> converted to number
+console.log(+"");   // 0    -> converted to number
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);   101
+
+let x = 5;
+console.log(++x);
+
+let y = 10;
+console.log(y++);
+console.log(y);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
